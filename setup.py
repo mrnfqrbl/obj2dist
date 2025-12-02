a8e46a0df7c1787f7f587368a061c319818b432c
MRNF <mrnfqrbl@gmail.com>
2025-12-03 02:22:19 +0800

    修复导入异常

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="obj2dict",        # 库名
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "pydantic",  # Pydantic 数据模型库
